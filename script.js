e477f0dcdc290bc5aeff1d0b57675115e3dff547
Isabella Sofia <isascoelhok@gmail.com>
2025-07-18 10:16:22 -0300

    versão botão salvar

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // Usamos 'document.getElementById' para pegar o elemento pelo 'id' que definimos no HTML.
     const blocoDeNotas = document.getElementById('blocoDeNotas');
     const btnLimparNotas = document.getElementById('btnLimparNotas');
+    const btnSalvar = document.getElementById('btnSalvar')
     // Adicionamos um evento de clique ao botão para limpar as notas
     btnLimparNotas.addEventListener('click', () => {
     // Quando o botão é clicado, limpamos o conteúdo do bloco de notas
@@ -17,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
     localStorage.removeItem('minhaNota');
     //Mensagem de confirmação no console
     console.log("Notas Limpas e removidas do localStorage");
-    })
+    });
     // 2. CARREGANDO DADOS DO LOCALSTORAGE
     // ------------------------------------
     // O 'localStorage' é um recurso do navegador que permite salvar informações
@@ -31,7 +32,8 @@ document.addEventListener('DOMContentLoaded', () => {
         // Se 'notaSalva' não for nulo (ou seja, existe algo salvo),
         // nós colocamos o valor salvo de volta no nosso 'blocoDeNotas'.
         blocoDeNotas.value = notaSalva;
-    }
+    };
+
 
     // 3. ADICIONANDO UM 'EVENTLISTENER'
     // ---------------------------------
@@ -43,7 +45,7 @@ document.addEventListener('DOMContentLoaded', () => {
     //     toda vez que o valor do <textarea> muda (ou seja, o usuário digita, apaga, etc).
     //   - O segundo é a FUNÇÃO que será executada quando o evento acontecer.
     //     Esta função é chamada de "callback".
-    blocoDeNotas.addEventListener('input', () => {
+    btnSalvar.addEventListener('click', () => {
         // 4. SALVANDO DADOS NO LOCALSTORAGE
         // -----------------------------------
         // Dentro da nossa função de callback, pegamos o valor atual do bloco de notas
